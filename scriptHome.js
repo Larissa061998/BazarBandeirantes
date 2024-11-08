19e800e66dea7d701b95ba17dd4532242761174d
Fernanda Martins Viotto <140853127+Fernandamviotto@users.noreply.github.com>
2024-11-07 23:44:14 -0300

    menuSanduiche

---
+++ scriptHome.js
@@ -0,0 +1,16 @@
+// Seleciona os elementos necessários
+const menuToggle = document.getElementById('menu-toggle');
+const menu = document.getElementById('menu');
+const produtosLink = document.querySelector('.menu-item-produtos');
+const submenu = document.querySelector('.submenu');
+
+// Exibe o menu lateral ao clicar no ícone de 3 linhas
+menuToggle.addEventListener('click', () => {
+    menu.classList.toggle('open');
+});
+
+// Exibe ou esconde o submenu ao clicar em "Produtos"
+produtosLink.addEventListener('click', (e) => {
+    e.preventDefault(); // Impede o redirecionamento padrão do link
+    submenu.classList.toggle('open-submenu');
+});
