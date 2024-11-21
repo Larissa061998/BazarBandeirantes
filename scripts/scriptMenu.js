5f88d4e90f0cff5c4c564aecdd62daa74699e31d
Fernanda Martins Viotto <140853127+Fernandamviotto@users.noreply.github.com>
2024-11-21 20:56:41 -0300

    MENU MOB

--- scripts/scriptMenu.js
+++ scripts/scriptMenu.js
@@ -1,17 +1,17 @@
 // Seleciona os elementos necessários
-const menuToggle = document.getElementById('menu-toggle');
-const menu = document.getElementById('menu');
-const produtosLink = document.querySelector('.menu-item-produtos');
-const submenu = document.querySelector('.submenu');
+var iconMenu = document.getElementById('icon-menu');
+var menu = document.getElementById('menumob');
+var produtosLink = document.querySelector('.menu-item-produtosmob');
+var submenu = document.querySelector('.submenumob');
 
-// Exibe o menu lateral ao clicar no ícone de 3 linhas
-menuToggle.addEventListener('click', () => {
-    menu.classList.toggle('open');
+iconMenu.addEventListener('click', () => {
+    menu.style.display = 'flex';
 });
 
 // Exibe ou esconde o submenu ao clicar em "Produtos"
 produtosLink.addEventListener('click', (e) => {
     e.preventDefault(); // Impede o redirecionamento padrão do link
-    submenu.classList.toggle('open-submenu');
+    submenumob.classList.toggle('open-submenumob');
 });
+
 
